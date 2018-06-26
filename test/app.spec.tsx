0c4552bd3ec862ad00845fdd8e8577e7545e5463
José Quintana <joseluisquintana20@gmail.com>
2018-06-26 22:52:21 +0200

    add testing

---
+++ test/app.spec.tsx
@@ -0,0 +1,43 @@
+import { h, app, View } from 'hyperapp'
+import { State, Actions } from '../src/types'
+import { state, actions } from '../src/app'
+import { CounterLabel } from '../src/components/counter'
+
+const myState: State = { count: 0 }
+
+test('state should be defined', () => {
+  expect(state).toBeTruthy()
+  expect(state).toEqual(expect.objectContaining(myState))
+})
+
+function equalTo (expected: string, done: Function) {
+  expect(document.body.innerHTML).toBe(expected)
+  done()
+}
+
+test('debouncing', (done: Function) => {
+  const view: View<State, Actions> = ({ count }, actions) => (
+    <main oncreate={() => equalTo('<main><h1>1</h1></main>', done)}>
+      <CounterLabel label={count} {...actions} />
+    </main >
+  )
+
+  app<State, Actions>(state, actions, view, document.body).up()
+})
+
+test('returning null from a component', (done) => {
+  const NullComponent = () => null
+
+  const view = () => (
+    <div
+      oncreate={() => {
+        expect(document.body.innerHTML).toBe('<div></div>')
+        done()
+      }}
+    >
+      <NullComponent />
+    </div>
+  )
+
+  app(null, null, view, document.body)
+})
